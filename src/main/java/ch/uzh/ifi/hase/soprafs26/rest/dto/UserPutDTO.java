9dc546f7f55508fc0df5c632b89492d95e463b31
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-03-26 11:07:57 +0100

    adding getters and setters to DTOs

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/UserPutDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/UserPutDTO.java
@@ -4,4 +4,30 @@ public class UserPutDTO {
     private String password;
     private String username;
     private String bio;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+
 }
