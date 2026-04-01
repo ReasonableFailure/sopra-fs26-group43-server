f87f0fd903ef941841ae0ea58c0342716cd6d268
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-04-01 02:05:23 +0200

    add mappers for scenario dtos

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/ScenarioDTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/ScenarioDTOMapper.java
@@ -1,8 +1,12 @@
 package ch.uzh.ifi.hase.soprafs26.rest.mapper;
+import ch.uzh.ifi.hase.soprafs26.rest.scenariodto.ScenarioDeleteDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.scenariodto.ScenarioGetDTO;
 import ch.uzh.ifi.hase.soprafs26.entity.Scenario;
+import ch.uzh.ifi.hase.soprafs26.rest.scenariodto.ScenarioPostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.scenariodto.ScenarioPutDTO;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Mapper
 public interface ScenarioDTOMapper {
@@ -16,5 +20,7 @@ public interface ScenarioDTOMapper {
     @Mapping(source = "exchangeRate", target = "exchangeRate")
     ScenarioGetDTO convertEntityToScenarioGetDTO(Scenario scenario);
 
+    Scenario convertScenarioPostDTOtoEntity(ScenarioPostDTO scenarioPostDTO);
 
+    Scenario convertScenarioPutDTOtoEntity(ScenarioPutDTO scenarioPutDTO);
 }
