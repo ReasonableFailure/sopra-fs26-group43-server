9fe4bf68222ec4d2a8720e80aeffbd47a6fcbb0e
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-04-01 02:56:50 +0200

    add requisite persistence annotations

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Player.java
@@ -2,10 +2,13 @@ package ch.uzh.ifi.hase.soprafs26.entity;
 import jakarta.persistence.*;
 
 @Inheritance(strategy = InheritanceType.JOINED)
+@Entity
 abstract public class Player {
     @Id
     private Long id;
 
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId")
     private User user;
 
     public Long getId() {
