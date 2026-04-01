6eb43a58915ddb383405c9dd75fbbe239d8fa172
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-04-01 02:50:35 +0200

    add endpoint stub todo

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/PlayerController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/PlayerController.java
@@ -1,11 +1,17 @@
 package ch.uzh.ifi.hase.soprafs26.controller;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import ch.uzh.ifi.hase.soprafs26.rest.playerdto.CharacterPutDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PlayerController {
 
-
+    @PutMapping("/characters/{characterId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void updateCharacter(@RequestBody CharacterPutDTO character, @PathVariable("characterId") long characterId) {
+        //TODO: implement stub
+    }
 
 }
