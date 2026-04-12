10e77b03aedd182a3e4ef47287f2467433d44895
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-04-12 18:38:57 +0200

    add automatic updating of fk in player db when assigning a user to a player

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/PlayerDTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/PlayerDTOMapper.java
@@ -1,12 +1,24 @@
 package ch.uzh.ifi.hase.soprafs26.rest.mapper;
 
+import ch.uzh.ifi.hase.soprafs26.entity.Player;
+import ch.uzh.ifi.hase.soprafs26.entity.User;
+import ch.uzh.ifi.hase.soprafs26.rest.playerdto.PlayerPutDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.playerdto.RoleGetDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.playerdto.RolePutDTO;
+import ch.uzh.ifi.hase.soprafs26.entity.Role;
 import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
-public interface PlayerDTOMapper {
-    Character convertCharacterPutDTOtoEntity(RolePutDTO rolePutDTO, @MappingTarget Character character);
-    RoleGetDTO convertEntitytoCharacterGetDTO(Character character);
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = PlayerMapper.class)
+ public interface PlayerDTOMapper {
+
+
+    PlayerDTOMapper INSTANCE = Mappers.getMapper(PlayerDTOMapper.class);
+
+    Role convertRolePutDTOtoEntity(RolePutDTO rolePutDTO, @MappingTarget Role role);
+
+    RoleGetDTO convertEntitytoRoleGetDTO(Role role);
+
+    Player convertPlayerPutDTOtoEntity(PlayerPutDTO playerPutDTO, @MappingTarget Player player);
 
 }
