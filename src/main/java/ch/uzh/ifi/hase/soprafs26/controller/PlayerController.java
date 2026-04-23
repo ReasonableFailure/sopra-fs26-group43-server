9ec79c8c37cd54f34011ddec30831a0c3ec5e81f
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-04-23 20:10:09 +0200

    messages can be bought: closes #112; likes are now tracked internally, adding backroomer to scenario endpoint: closes #14

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/PlayerController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/PlayerController.java
@@ -1,17 +1,14 @@
 package ch.uzh.ifi.hase.soprafs26.controller;
 
+import ch.uzh.ifi.hase.soprafs26.entity.Backroomer;
 import ch.uzh.ifi.hase.soprafs26.entity.Role;
 import ch.uzh.ifi.hase.soprafs26.rest.mapper.PlayerDTOMapper;
-import ch.uzh.ifi.hase.soprafs26.rest.playerdto.PlayerPutDTO;
-import ch.uzh.ifi.hase.soprafs26.rest.playerdto.RoleGetDTO;
-import ch.uzh.ifi.hase.soprafs26.rest.playerdto.RolePostDTO;
-import ch.uzh.ifi.hase.soprafs26.rest.playerdto.RolePutDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.playerdto.*;
 import ch.uzh.ifi.hase.soprafs26.service.PlayerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,7 +18,9 @@ public class PlayerController {
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
     }
+
     @PutMapping("/player/{playerId}")
+    @ResponseStatus(HttpStatus.OK)
     public void assignUserToPlayer(@PathVariable Long playerId, @RequestBody PlayerPutDTO playerPutDTO,  @RequestHeader("Authorization") String token) {
         String[] tokens = splitToken(token);
         if(tokens[0].equals("Bearer")){
@@ -31,10 +30,42 @@ public class PlayerController {
         }
     }
 
+    @PostMapping("/characters")
+    @ResponseStatus(HttpStatus.OK)
+    public RoleGetDTO createRole(@RequestBody RolePostDTO rolePostDTO, @RequestHeader("Authorization") String authToken){
+        String[] tokens = splitToken(authToken);
+        if(tokens[0].equals("Director")){
+            Role role = playerService.createRole(tokens[1], rolePostDTO);
+            return PlayerDTOMapper.INSTANCE.convertEntitytoRoleGetDTO(role);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping("/backroomers")
+    @ResponseStatus(HttpStatus.OK)
+    public PlayerGetDTO createBackroomer(@RequestHeader("Authorization") String token){
+        String[] tokens = splitToken(token);
+        if(tokens[0].equals("Bearer")){
+            Backroomer b = playerService.createBackroomer(tokens[1]);
+            return PlayerDTOMapper.INSTANCE.convertEntitytoPlayerGetDTO(b);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @DeleteMapping("/characters/{characterId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteRole(@RequestHeader("Authorization")String token, @PathVariable Long characterId){
+        String[] tokens = splitToken(token);
+        if (tokens[0].equals("Director")){
+            playerService.deleteRole(token,characterId);
+        } else throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+
     @PutMapping("/characters/{characterId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ResponseBody
-
     public void updateRole(@RequestBody RolePutDTO rolePutDTO, @RequestHeader("Authorization") String token, @PathVariable Long characterId) {
         String[] tokens = splitToken(token);
         if(tokens[0].equals("Director")){
@@ -44,34 +75,23 @@ public class PlayerController {
         }
     }
 
+    @PutMapping("/character/{characterId}/points")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateLikeCounter(@PathVariable Long characterId, @RequestBody RoleIncrementationDTO roleIncrementationDTO, @RequestHeader("Authorization") String token) {
+        String[] tokens = splitToken(token);
+        if(tokens[0].equals("Role")){
+            playerService.addLikes(tokens[1], characterId,roleIncrementationDTO.getIncrementBy());
+        }
+    }
+
     @GetMapping("/characters/{characterId}/detail")
-    @ResponseStatus
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public RoleGetDTO getRole(@RequestHeader("Authorization") String token, @PathVariable Long characterId) {
         String[] tokens = splitToken(token);
         return PlayerDTOMapper.INSTANCE.convertEntitytoRoleGetDTO(playerService.getRole(tokens[1],characterId));
     }
 
-    @PostMapping("/characters")
-    public RoleGetDTO createRole(@RequestBody RolePostDTO rolePostDTO, @RequestHeader("Authorization") String authToken){
-        String[] tokens = splitToken(authToken);
-        if(tokens[0].equals("Director")){
-            return PlayerDTOMapper.INSTANCE.convertEntitytoRoleGetDTO(playerService.createRole(tokens[1], rolePostDTO));
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-    @DeleteMapping("/characters/{characterId}")
-    public void deleteRole(@RequestHeader("Authorization")String token, @PathVariable Long characterId){
-        String[] tokens = splitToken(token);
-        if (tokens[0].equals("Director")){
-            playerService.deleteRole(token,characterId);
-        } else throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }
-
-
-  
     @GetMapping("/characters/{scenarioId}/{characterId}/interlocutors")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
@@ -85,7 +105,11 @@ public class PlayerController {
                 .map(PlayerDTOMapper.INSTANCE::convertEntitytoRoleGetDTO)
                 .toList();
     }
-  
+
+
+
+
+
      public static String[] splitToken(String token){
         if(token == null || token.isEmpty()){
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token is empty");
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/scenariodto/ScenarioPlayerDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs26.rest.scenariodto;
+
+public class ScenarioPlayerDTO {
+    private Long toAssignId;
+
+    public Long getToAssignId() {
+        return toAssignId;
+    }
+
+    public void setToAssignId(Long toAssignId) {
+        this.toAssignId = toAssignId;
+    }
+}
